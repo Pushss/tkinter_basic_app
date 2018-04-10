e4778e9edec031826c719dbacf096b755ffecb9b
Pushss <chrisjamescam10@gmail.com>
2018-04-10 21:38:43 +0100

    Kg to grams,pounts,ounces calculator

--- script.py
+++ script.py
@@ -2,18 +2,32 @@ from tkinter import * #import all from tkinter libary. allows use of Button() in
 
 window=Tk() #create window
 
-def km_to_miles():
-    miles = float(e1_value.get()) * 1.6 #convert e1_value to float multiply by 1.6
-    t1.insert(END,miles) #insert something in to t1 ,param's where to put text, e1_value variable wanting to insert
+def conversion():
+    value = e1_value.get()
+    grams= float(value) * 1000 #convert e1_value to float multiply by 1.6
+    pounds=float(value) * 2.20462
+    ounces=float(value) * 35.274
+    t1.insert(END,grams) #insert something in to t1 ,param's where to put text, e1_value variable wanting to insert
+    t2.insert(END,pounds)
+    t3.insert(END,ounces)
 
-b1=Button(window, text="Execute",command=km_to_miles) #create button (places in window, text="String",command=call's function without ())
-b1.grid(row=0,column=0) #places the button on the window can define postion with .grid(grid can have row=0, column=0 for easier placement, rowspan=number of grid row the button will span) or .pack
+l1=Label(window, text="Kg:")
+l1.grid(row=0,column=0)
 
 e1_value=StringVar() #declare string variable to store Entry() data
 e1=Entry(window, textvariable=e1_value) #entry widget (editText)(textvariable = e1_value)
 e1.grid(row=0,column=1) #postions entry widget on grid
 
+b1=Button(window, text="Convert",command=conversion) #create button (places in window, text="String",command=call's function without ())
+b1.grid(row=0,column=2) #places the button on the window can define postion with .grid(grid can have row=0, column=0 for easier placement, rowspan=number of grid row the button will span) or .pack
+
 t1=Text(window,height=1,width=20) #text widget(textView)
-t1.grid(row=0,column=2) #postions text widget on grid
+t1.grid(row=1,column=0) #postions text widget on grid
+
+t2=Text(window,height=1,width=20) #text widget(textView)
+t2.grid(row=1,column=1) #postions text widget on grid
+
+t3=Text(window,height=1,width=20) #text widget(textView)
+t3.grid(row=1,column=2) #postions text widget on grid
 
 window.mainloop() #allows window to remain open permentaly keep this at end of code
